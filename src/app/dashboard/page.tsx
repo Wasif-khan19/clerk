e6589fe4d23e6a786e69d628b90dd1c75c8b3c55
Wasif <wk5599426@gmail.com>
2024-03-06 16:26:05 +0500

    new

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { auth, currentUser } from '@clerk/nextjs';
+
+export default async function DashboardPage() {
+  const { userId } = auth();
+  const user = await currentUser();
+
+  if (!userId || !user) {
+    return <div>You are not logged in</div>;
+  }
+
+  return (
+    <div></div>
+  );
+}
